3cb5b53813ad4ff1317a5a9e848ee3ab4fe07ffd
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2024-03-04 04:40:20 -0500

    primary

---
+++ piensa-semana7.1/src/test/kotlin/com/example/piensasemana71/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.piensasemana71
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
